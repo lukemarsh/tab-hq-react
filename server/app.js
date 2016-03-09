90a9e632b0684b2be70621fbda98807c4473ec2f
Luke Marsh <luke@theappbusiness.com>
2016-03-09 08:46:39 +0000

    specifying node version

--- server/app.js
+++ server/app.js
@@ -109,7 +109,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  // next();
+  next();
 });
 
 // app.use('/categories', ensureApiAuthenticated, categories);
