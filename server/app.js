a8115154be9e9843372f63f85ccace1c7db1965f
Luke Marsh <luke@theappbusiness.com>
2016-03-09 07:25:18 +0000

    commenting out express path

--- server/app.js
+++ server/app.js
@@ -103,7 +103,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/../build')));
+// app.use(express.static(path.join(__dirname, '/../build')));
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
